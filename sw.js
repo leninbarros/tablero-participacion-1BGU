2bcd7113296082469dcc3307e4353bbfdf403b66
leninbarros <65048266+leninbarros@users.noreply.github.com>
2026-08-28 17:24:36 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-
+// Service Worker requerido por Android Chrome para modo Standalone
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
